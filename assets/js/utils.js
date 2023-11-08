6c93e369d118ac16730efa7ae4bd5e63afccf416
unknown <leandrodfreitas@gmail.com>
2023-11-08 16:37:48 -0300

    incluindo video

---
+++ assets/js/utils.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    $('#carousel-promo').owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 300,
+        nav: false,
+        responsive: {
+            0:{
+                items: 1
+            },
+            600:{
+                items: 1
+            },
+            1000:{
+                items: 1
+            }
+        }
+    });
+
+});
